484401757659b4daca2a9394d80ba920158ba8f3
Taron Kheboyan <taron-x85@outlook.com>
2022-05-13 01:27:30 +0400

    invested price

--- models/adModel.js
+++ models/adModel.js
@@ -1,21 +1,30 @@
 const mongoose = require('mongoose');
+const currentAd = require('./currentAd');
 const { Schema } = mongoose;
 
 const adSchema = new Schema({
+    _id: Schema.Types.ObjectId,
     name: { type: String, required: true },
     startDate: { type: Date, default: Date.now() },
     endDate: { type: Date, default: Date.now() },
     dayCount: { type: Number, default: 1 },
+    priceForPerView: {
+        type: Number,
+        get: v => (v / 100).toFixed(2),
+        set: v => v * 100,
+        required: true
+    },
     price: {
         type: Number,
         get: v => (v / 100).toFixed(2),
         set: v => v * 100,
         required: true
     },
-    countDailyView: { type: number, default: 1 }
+    countDailyView: { type: Number, default: 1 },
+    currentAd: [{ type: Schema.Types.ObjectId, ref: 'current_ad' }]
 },
     {
         toJSON: { getters: true }
     });
 
-module.exports = mongoose.model('Ad', adSchema);
+module.exports = mongoose.model('ad', adSchema);
